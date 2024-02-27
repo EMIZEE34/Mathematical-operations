688cf99aff787b82189a873c2296456e2144340e
Amna Muhammad Yameen <160321177+EMIZEE34@users.noreply.github.com>
2024-02-27 19:40:27 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var Mathematicial = "I made a Mathematicial opertions you can see below :";
+console.log(Mathematicial);
+var a = 200;
+var b = 400;
+//1 way//
+console.log("The Sum of a + b is =", a + b);
+console.log("The Sub of a - b is =", a - b);
+console.log("The multi of a * b is =", a * b);
+console.log("The divi of a / b is =", a / b);
+//2 way//
+console.log("The Sum of a + b is ".concat(a + b));
+console.log("The sub of a - b is ".concat(a - b));
+console.log("The multi of a * b is ".concat(a * b));
+console.log("The divi of a / b is ".concat(a / b));
---
+++ index.ts
@@ -0,0 +1,19 @@
+let Mathematicial: string = "I made a Mathematicial opertions you can see below :";
+console.log( Mathematicial);
+
+let a: number = 200;
+let b: number = 400;
+
+//1 way//
+
+console.log("The Sum of a + b is =", a+b);
+console.log("The Sub of a - b is =", a-b);
+console.log("The multi of a * b is =", a*b);
+console.log("The divi of a / b is =", a/b);
+
+//2 way//
+
+console.log(`The Sum of a + b is ${a+b}`);
+console.log(`The sub of a - b is ${a-b}`);
+console.log(`The multi of a * b is ${a*b}`);
+console.log(`The divi of a / b is ${a/b}`);
